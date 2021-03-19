b8c9bd01a48a0b070d934438bbf1202858e7ed5a
abhay-hisesci <abhay@hisescientific.com>
2021-03-18 23:29:37 -0400

    Feature: Setup calculator with addition property

---
+++ src/calculator.py
@@ -0,0 +1,9 @@
+class calculator:
+    result = 0
+    def __init__(self):
+        self.result = 1;
+        pass
+
+    def add(self, a, b):
+        c = a + b
+        return c
--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -8,7 +8,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_results_property(self):
         calc = calculator()
-        self.assertEqual(calc.result, 3)
+        self.assertEqual(calc.result, 1)
+
+    def test_addition(self):
+        calc = calculator()
+        self.assertEqual(calc.add(1, 1), 2)
 
 if __name__ == '__main__':
     unittest.main()
